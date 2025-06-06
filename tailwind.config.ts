a1657f102f4d95691131ccf61d9ade1cdd32fe77
hyewonny2327 <wonny2327@gmail.com>
2025-06-06 14:13:15 +0900

    setup : #4 tailwind 색상 테마 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,20 @@ const config: Config = {
       fontFamily: {
         pretendard: ['var(--font-pretendard)'],
       },
+      colors: {
+        background: 'var(--color-background)',
+        muted: 'var(--color-muted)',
+        primary: 'var(--color-primary)',
+        accent: 'var(--color-accent)',
+        foreground: 'var(--color-foreground)',
+        heading: 'var(--color-heading)',
+        border: 'var(--color-border)',
+        highlight: 'var(--color-highlight)',
+      },
     },
   },
   plugins: [],
 };
 
 export default config;
+// tailwind.config.js or tailwind.config.ts
